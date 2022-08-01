56d64217feb0aecd9007f023f40b1250bdeeec33
jpoliveiradev <info1joaopaulo@gmail.com>
2022-08-01 11:29:28 -0300

    first commit

---
+++ Library.API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Library.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.API.Data {
+    public class DataContext : DbContext {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Editoras> Editoras { get; set; }
+        public DbSet<Livros> Livros { get; set; }
+        public DbSet<Alugueis> Alugueis { get; set; }
+        public DbSet<Login> Login { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder buider) {
+            //buider.Entity<Clientes>();
+        }
+
+    }
+}
--- Library.API/Startup.cs
+++ Library.API/Startup.cs
@@ -1,6 +1,8 @@
+using Library.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,7 +22,18 @@ namespace Library.API {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
+
+            services.AddDbContext<DataContext>(
+                context => context.UseSqlite(Configuration.GetConnectionString("Default"))
+
+
+            );
+
+
             services.AddControllers();
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
